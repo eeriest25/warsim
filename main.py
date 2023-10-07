d65dc8373294ad0ef85b99ee127f7c95935250e8
eeriest25 <itsnerfornothin1029384756@gmail.com>
2023-10-07 12:04:22 -0700

    removed webhooks
    it was worth a try

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 import discord
-from discord import Webhook
-import aiohttp
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,27 +8,13 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-'''async def foo():
-    async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url('https://discord.com/api/webhooks/1160286689302958151/D447xqp2F60scvSu-21_QbTgoXwSqz3QaFbsbs1Kr37FjVQRacwbsyKCzZ972GfieK_h)',
-                                   session=session)
-        await webhook.send('Hello World', username ='Foo')'''
-
-@client.event
-async def on_message():
-    async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url('https://discord.com/api/webhooks/1160286689302958151/D447xqp2F60scvSu-21_QbTgoXwSqz3QaFbsbs1Kr37FjVQRacwbsyKCzZ972GfieK_h)',
-                                   session=session)
-        await webhook.send('Hello World', username ='Foo')
-    await Webhook.send(content='wassup')
-
 @client.event
 async def on_ready():
     print(f'{client.user} is now running!')
 
 @client.event
 async def on_message(message):
-    '''if message.author != client.user:
+    if message.author != client.user:
         print(message.author)
         print(message.channel)
         print(message.content)
@@ -37,7 +22,7 @@ async def on_message(message):
         if response == 'none':
             pass
         else:
-            await message.channel.send(response)'''
+            await message.channel.send(response)
 
 
 
